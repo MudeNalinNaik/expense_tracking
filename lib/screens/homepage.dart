887e128dc815d29efd21fbf51ed43bba217ff374
Nalin <nalinnaikmude@gmail.com>
2024-05-07 22:02:39 +0530

    flashscreen & homepageui created

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Expanses'),
+      ),
+      body: const Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('320 rs'),
+            subtitle:Text('Groccesories') ,
+          ),
+          ListTile(
+            title: Text('180 rs'),
+            subtitle:Text('Eggs') ,
+          ),
+          ListTile(
+            title: Text('250 rs'),
+            subtitle:Text('Chicken') ,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:expense_tracking/screens/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyHomePage()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Loading...',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
