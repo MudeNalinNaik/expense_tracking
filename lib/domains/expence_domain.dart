e7e8d5c081ffb0c5ac1e064329f26d8f4f6ea322
chandraobulareddy <dchandrachaan@gmail.com>
2024-05-08 21:47:49 +0530

    domain added

---
+++ lib/domains/expence_domain.dart
@@ -0,0 +1,7 @@
+class Expense {
+  final String title;
+  final String money;
+  final DateTime date;
+
+  Expense({required this.title, required this.money, required this.date});
+}
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
+import '../domains/expence_domain.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -11,6 +13,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final titleController = TextEditingController();
+  final moneyController = TextEditingController();
+
   DateTime selectedDate = DateTime.now();
 
   Future<void> _selectDate(BuildContext context) async {
@@ -26,6 +31,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  List<Expense> expenses = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,130 +40,132 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Expanses'),
       ),
-      body: const Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('320 rs'),
-            subtitle: Text('Groccesories'),
-          ),
-          ListTile(
-            title: Text('180 rs'),
-            subtitle: Text('Eggs'),
-          ),
-          ListTile(
-            title: Text('250 rs'),
-            subtitle: Text('Chicken'),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            for (int a = 0; a < expenses.length; a++)
+              ListTile(
+                title: Text(expenses[a].money),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(expenses[a].title),
+                    Text(
+                        "${expenses[a].date.year.toString()}-${expenses[a].date.month.toString().padLeft(2, '0')}-${expenses[a].date.day.toString().padLeft(2, '0')}"),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showDialog(
-              context: context,
-              builder: (_) => StatefulBuilder(
-                  builder: (context, setState) {
-                    DateTime selectedDate = DateTime.now();
-                    return AlertDialog(
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Row(
-                              children: [
-                                const Expanded(flex: 1, child: Text('Money : ')),
-                                Expanded(
-                                    flex: 3,
-                                    child: TextField(
-                                      keyboardType: TextInputType.number,
-                                      inputFormatters: <TextInputFormatter>[
-                                        FilteringTextInputFormatter.allow(
-                                            RegExp(r'[0-9]')),
-                                        FilteringTextInputFormatter.digitsOnly
-                                      ],
-                                      decoration: InputDecoration(
-                                        prefixIcon:
-                                            const Icon(Icons.monetization_on),
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(16.0),
-                                        ),
-                                        hintStyle:
-                                            const TextStyle(color: Colors.grey),
-                                        hintText: "Type in your text",
-                                        fillColor: Colors.white70,
-                                      ),
-                                    ))
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                const Expanded(flex: 1, child: Text('Title : ')),
-                                Expanded(
-                                    flex: 3,
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(16.0),
-                                        ),
-                                        hintStyle:
-                                            const TextStyle(color: Colors.grey),
-                                        hintText: "Type in your text",
-                                        fillColor: Colors.white70,
-                                      ),
-                                    ))
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                const Expanded(flex: 1, child: Text('Date : ')),
-                                Expanded(
-                                    flex: 3,
-                                    child: InkWell(
-                                      onTap: () {
-
-                                        Future<void> _selectDate(BuildContext context) async {
-                                          final DateTime? picked = await showDatePicker(
-                                              context: context,
-                                              initialDate: selectedDate,
-                                              firstDate: DateTime(2015, 8),
-                                              lastDate: DateTime(2101));
-                                          if (picked != null && picked != selectedDate) {
-                                            setState(() {
-                                              selectedDate = picked;
-                                            });
-                                          }
-                                        }
-                                      },
-                                      child: Text(
-                                        "${selectedDate.year.toString()}-${selectedDate.month.toString().padLeft(2,'0')}-${selectedDate.day.toString().padLeft(2,'0')}",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium
-                                            ?.copyWith(color: Colors.blue),
-                                      ),
-                                    ))
-                              ],
-                            ),
-                          ],
-                        ),
-                        actions: [
-                          TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Okay'))
-                        ],
-                      );
-                }
-              ));
+          addItem();
         },
         tooltip: 'Add New',
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  addItem() {
+    showDialog(
+        context: context,
+        builder: (_) => StatefulBuilder(builder: (context, setState) {
+              return AlertDialog(
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        const Expanded(flex: 1, child: Text('Money : ')),
+                        Expanded(
+                            flex: 3,
+                            child: TextField(
+                              controller: moneyController,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9]')),
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              decoration: InputDecoration(
+                                prefixIcon: const Icon(Icons.monetization_on),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                hintText: "Type in your text",
+                                fillColor: Colors.white70,
+                              ),
+                            ))
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(flex: 1, child: Text('Title : ')),
+                        Expanded(
+                            flex: 3,
+                            child: TextField(
+                              controller: titleController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                hintText: "Type in your text",
+                                fillColor: Colors.white70,
+                              ),
+                            ))
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(flex: 1, child: Text('Date : ')),
+                        Expanded(
+                            flex: 3,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "${selectedDate.year.toString()}-${selectedDate.month.toString().padLeft(2, '0')}-${selectedDate.day.toString().padLeft(2, '0')}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(color: Colors.blue),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+
+                        setState(() {
+                          expenses.add(Expense(
+                              title: titleController.text,
+                              money: moneyController.text,
+                              date: DateTime.now()));
+                          titleController.clear();
+                          moneyController.clear();
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Okay')),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel')),
+                ],
+              );
+            }));
   }
 }
